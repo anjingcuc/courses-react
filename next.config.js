ef33d45b440dfaa616c04f01b1a52d9ba333bdd6
anjing <anjing@cuc.edu.cn>
2026-03-02 18:44:48 +0800

    fix: 更新 basePath 为 /courses-react

--- next.config.js
+++ next.config.js
@@ -1,7 +1,7 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   output: 'export',
-  basePath: '/courses-wiki',
+  basePath: '/courses-react',
   images: {
     unoptimized: true,
   },
